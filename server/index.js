8ff609b59892560fc5189656cefb1bd605526e4b
Olga Mokshantseva <mokshano@oregonstate.edu>
2025-10-19 13:51:21 -0700

    remove test string

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Express.js API!' });
+  res.json({ message: '' });
 });
 
 // User routes
